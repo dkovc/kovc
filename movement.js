24887b483725b47feb91587562421eed60a7cd18
dkovc <d@ko.vc>
2024-05-11 20:12:04 +0400

    Update movement.js

--- movement.js
+++ movement.js
@@ -1,5 +1,5 @@
 document.body.addEventListener('mousemove', function(e) {
-    const x = e.clientX / window.innerWidth - 0.5;
-    const y = e.clientY / window.innerHeight - 0.5;
+    const x = e.clientX / window.innerWidth * 2 - 1;
+    const y = e.clientY / window.innerHeight * 2 - 1;
     document.body.style.backgroundPosition = `${x * 30}px ${y * 30}px, ${-x * 30}px ${-y * 30}px`;
 });
