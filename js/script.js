a1dbdbd409a3276b93f6427e37136d324df83184
usvx <d@ko.vc>
2024-06-14 19:00:28 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,6 +6,6 @@ function redirectToGoogleLogin(event) {
     event.preventDefault();
     const username = document.querySelector('input[name="username"]').value;
     const email = username + '@ko.vc';
-    const loginUrl = `https://accounts.google.com/ServiceLogin?Email=${encodeURIComponent(email)}`;
+    const loginUrl = `https://accounts.google.com/a/ko.vc/ServiceLogin?Email=${encodeURIComponent(email)}`;
     window.location.href = loginUrl;
 }
