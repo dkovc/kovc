d2d61a015073e69bf5d7109e73dc575fafd0ae89
usvx <d@ko.vc>
2024-06-17 09:33:47 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,13 +10,26 @@ document.addEventListener('DOMContentLoaded', () => {
         sphere.style.background = 'radial-gradient(circle at 50% 50%, var(--button-bg-start), var(--button-bg-end) 70%)';
     });
 
-    document.getElementById('login-form').addEventListener('submit', redirectToGoogleLogin);
+    document.getElementById('login-form').addEventListener('submit', handleFormSubmit);
 });
 
-function redirectToGoogleLogin(event) {
+function handleFormSubmit(event) {
     event.preventDefault();
     const username = document.querySelector('input[name="username"]').value;
     const email = username + '@ko.vc';
+    const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+    
+    const form = event.target;
+    const hiddenInput = document.createElement('input');
+    hiddenInput.type = 'hidden';
+    hiddenInput.name = '_csrf';
+    hiddenInput.value = csrfToken;
+    form.appendChild(hiddenInput);
+
+    redirectToGoogleLogin(email);
+}
+
+function redirectToGoogleLogin(email) {
     const loginUrl = `https://accounts.google.com/AccountChooser?Email=${encodeURIComponent(email)}&continue=https://mail.google.com/a/ko.vc`;
     window.location.href = loginUrl;
 }
